8db4478ebac292e2850ea48cdf5fdaa15acb76da
mambaur <alwiroziq1998@gmail.com>
2022-07-28 23:04:42 +0700

    update infinite list in income and expense screen

--- lib/screens/transactions/expense_screen.dart
+++ lib/screens/transactions/expense_screen.dart
@@ -15,82 +15,112 @@ class ExpenseScreen extends StatefulWidget {
 
 class _ExpenseScreenState extends State<ExpenseScreen> {
   final TransactionRepository _transactionRepo = TransactionRepository();
+
+  final ScrollController _scrollController = ScrollController();
+
+  List<TransactionModel> transactions = [];
+  int page = 1;
+  int limit = 15;
+  bool hasReachedMax = false;
+
+  Future getTransactions() async {
+    List<TransactionModel> data =
+        await _transactionRepo.all(type: 'expense', page: page, limit: limit);
+
+    if (data.isEmpty || data.length < limit) {
+      hasReachedMax = true;
+    }
+    transactions.addAll(data);
+    setState(() {});
+  }
+
+  void onScroll() {
+    double maxScroll = _scrollController.position.maxScrollExtent;
+    double currentScroll = _scrollController.position.pixels;
+
+    if (currentScroll == maxScroll && !hasReachedMax) {
+      page++;
+      getTransactions();
+    }
+  }
+
+  @override
+  void initState() {
+    getTransactions();
+
+    _scrollController.addListener(onScroll);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Semua Pengeluaran'),
-        centerTitle: true,
-        elevation: 0.5,
-      ),
-      // backgroundColor: Colors.white,
-      body: FutureBuilder<List<TransactionModel>>(
-          future: _transactionRepo.all(type: 'expense'),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  physics: const BouncingScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onLongPress: () async {
-                        _optionEditDialog(snapshot.data![index]);
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.all(15),
-                        margin: const EdgeInsets.only(bottom: 10),
-                        color: Colors.white,
-                        child: Row(
+        appBar: AppBar(
+          title: const Text('Semua Pengeluaran'),
+          centerTitle: true,
+          elevation: 0.5,
+        ),
+        // backgroundColor: Colors.white,
+        body: ListView.builder(
+            controller: _scrollController,
+            itemCount:
+                hasReachedMax ? transactions.length : transactions.length + 1,
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              if (index < transactions.length) {
+                return GestureDetector(
+                  onLongPress: () async {
+                    _optionEditDialog(transactions[index]);
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    margin: const EdgeInsets.only(bottom: 10),
+                    color: Colors.white,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                            child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Expanded(
-                                child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  DateInstance.id(snapshot.data![index].date!),
-                                  style: TextStyle(
-                                      fontSize: 12, color: Colors.grey),
-                                ),
-                                SizedBox(
-                                  height: 3,
-                                ),
-                                Text(
-                                  snapshot.data![index].category!.name ?? '',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16),
-                                ),
-                                SizedBox(
-                                  height: 3,
-                                ),
-                                snapshot.data![index].notes != ''
-                                    ? Text(
-                                        'Catatan: ${snapshot.data![index].notes}')
-                                    : Container(),
-                              ],
-                            )),
                             Text(
-                                currencyId
-                                    .format(snapshot.data![index].nominal),
-                                style: TextStyle(
-                                    color: Colors.orange.shade500,
-                                    fontWeight: FontWeight.bold))
+                              DateInstance.id(transactions[index].date!),
+                              style:
+                                  TextStyle(fontSize: 12, color: Colors.grey),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              transactions[index].category!.name ?? '',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            transactions[index].notes != ''
+                                ? Text('Catatan: ${transactions[index].notes}')
+                                : Container(),
                           ],
-                        ),
-                      ),
-                    );
-                  });
-            } else {
-              return Center(
-                child: CircularProgressIndicator(
-                  color: Colors.green.shade700,
-                  strokeWidth: 2,
-                ),
-              );
-            }
-          }),
-    );
+                        )),
+                        Text(currencyId.format(transactions[index].nominal),
+                            style: TextStyle(
+                                color: Colors.orange.shade500,
+                                fontWeight: FontWeight.bold))
+                      ],
+                    ),
+                  ),
+                );
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.green.shade700,
+                    strokeWidth: 2,
+                  ),
+                );
+              }
+            }));
   }
 
   Future<void> _optionEditDialog(TransactionModel transactionModel) async {
